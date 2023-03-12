4b49a43e9c3c7a7d669b9cc5dd334f5788a6fc83
Olivia Barnett <olivia.barnett@gemini.com>
2023-03-12 18:59:32 -0400

    planning for resy integration

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,10 +1,11 @@
 import os
 import openai
-
+from datetime import date
 from flask import Flask, render_template, request
 
+MAX_TOKENS=50
 
-class Chat:
+class ChatGPTClient:
     def __init__(self):
         # Set up OpenAI API credentials
         openai.api_key = os.getenv("OPENAI_API_KEY")
@@ -12,28 +13,55 @@ class Chat:
         self.model = "gpt-3.5-turbo"
 
         # ChatGpt doesn't keep track of a conversation. We have to send it the whole conversation each time.
-        self.messages = [{"role": "system", "content": "You are a friendly assistant bot built to help users find and create reservations to restaurants"}]
+        self.messages = [{"role": "system", "content": "You are a bot built to help users find and create reservations to restaurants."}]
         
         response = openai.ChatCompletion.create(
             model=self.model,
             messages= self.messages,
-            max_tokens=20
+            max_tokens=MAX_TOKENS
         )
         
         self.messages.append({"role": "assistant", "content": response["choices"][0]["message"].content})
 
     def send_message(self, message):
+        print("DEBUG: messages = ", self.messages)
 
         self.messages.append({"role": "user", "content": message})
 
         response = openai.ChatCompletion.create(
             model=self.model,
             messages=self.messages,
-            max_tokens=20,
+            max_tokens=MAX_TOKENS,
         )
         
         self.messages.append({"role": "assistant", "content": response["choices"][0]["message"].content})
         return response["choices"][0]["message"].content
+
+class ResyClient:
+
+    # http://subzerocbd.info/#introduction
+    def __init__(self):
+        self.baseUrl = "https://api.resy.com"
+
+        # TODO: where do I get these
+        self.headers = {
+            'X-Resy-Auth-Token': os.getenv("RESY_AUTH_TOKEN"),
+            'Authorization': os.getenv("RESY_API_KEY")
+        }
+
+
+    # TODO: pass in date/time + location?
+    def findOpenReservations(partySize):
+        day = date.today().strftime("%Y/%m/%d")
+        params = {
+            'lat': '40.696235726060294',
+            'long': '-73.97968099999999',
+            'day': day,
+            'party_size': str(partySize)
+        }
+        response = requests.get(url, params=params, headers=headers)
+
+
 
 def app_set_up(test_config):
     app = Flask(__name__, instance_relative_config=True)
@@ -61,8 +89,8 @@ def create_app(test_config=None):
     # create and configure the app
     app = app_set_up(test_config)
 
-    # Set up chat with chatGpt
-    chatGpt = Chat()
+    chatGptClient = ChatGPTClient()
+    resyClient = ResyClient()
 
 
     @app.route('/')
@@ -71,11 +99,17 @@ def create_app(test_config=None):
 
     @app.route("/chat", methods=["POST"])
     def chat():
+
+        # TODO: We somehow need to take this input, determine if enough information has been given yet to make a call into resy and retrieve information
+        # Perhaps use another ChatGPT client that only goes from english text --> resy api calls? Then return that info, feed it into the 
+        # other chat gpt client which will convert from json resy data --> english response?
         user_input = request.form["input_text"]
 
-        bot_response = chatGpt.send_message(user_input)
 
-        # Return the bot's response as a JSON object
+        bot_response = chatGptClient.send_message(user_input)
+        
+
+
         return {"response": bot_response}
 
 
